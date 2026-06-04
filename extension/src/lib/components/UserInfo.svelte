3cff339fb9aaae968b3b363374e25d52fe3cbcc3
Allan Lasser <lasser.allan@gmail.com>
2026-06-04 12:54:45 -0400

    Links user to MuckRock Accounts

--- extension/src/lib/components/UserInfo.svelte
+++ extension/src/lib/components/UserInfo.svelte
@@ -2,24 +2,27 @@
   import { CircleUserRound } from '@lucide/svelte';
   import { authState } from '../auth.svelte'
 
+  const ACCOUNTS_HOST = import.meta.env.MUCKROCK_ACCOUNTS_HOST;
+
   const user = $derived(authState.user);
-  const name = $derived(user ? 
+  const name = $derived(user ?
     user?.preferred_username ??
     user?.name ??
     user?.email ??
     "Squarelet user"
     : "Anonymous"
   )
+  const profileUrl = $derived(`${ACCOUNTS_HOST}users/${user?.nickname}`);
 </script>
 
-<div class="user">
+<a class="user" href={profileUrl} target="_blank" rel="noopener noreferrer">
   {#if user?.picture}
     <img height="24" width="24" class="userAvatar" src={user.picture} alt={user.name} />
   {:else}
     <CircleUserRound size="24" />
   {/if}
   <span class="userName">{name}</span>
-</div>
+</a>
 
 <style>
   .user {
@@ -27,6 +30,11 @@
     align-items: center;
     gap: 0.5em;
     padding: 0 0.325em;
+    color: inherit;
+    text-decoration: none;
+  }
+  .user:hover .userName {
+    text-decoration: underline;
   }
   .userAvatar {
     border-radius: 1em;
